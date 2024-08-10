ecff5c9b967a7b904c40d8d619c74784265a64da
marky376 <mmutuamark@gmail.com>
2024-08-10 13:33:27 +0300

    Session Authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -11,21 +11,22 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_db_auth import SessionDBAuth
-from api.v1.auth.session_exp_auth import SessionExAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = SessionAuth()
+auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
-if  auth_type == 'session_auth':
+if auth_type == 'session_auth':
     auth = SessionAuth()
 if auth_type == 'session_exp_auth':
-    auth = SessionExAuth()
+    auth = SessionExpAuth()
 if auth_type == 'session_db_auth':
     auth = SessionDBAuth()
 
@@ -57,16 +58,15 @@ def authenticate_user():
     """
     if auth:
         excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/',
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
         ]
         if auth.require_auth(request.path, excluded_paths):
             user = auth.current_user(request)
-            
             if auth.authorization_header(request) is None and \
-                    auth.session_cookies(request) is None:
+                    auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
